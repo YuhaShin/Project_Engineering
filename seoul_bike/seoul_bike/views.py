567d46b8208ba3d4a60e1262b45373fd5bf1fe23
MoonYoungHo <ansdudgh6130@naver.com>
2022-08-16 09:15:45 +0900

    c

--- seoul_bike/seoul_bike/views.py
+++ seoul_bike/seoul_bike/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
+from . import visual
 from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    contexts = dict()
+    contexts['countStationId'] = visual.countStationId()
+    contexts['topStation_id'] = visual.topStation_id()
+    return render(request, 'index.html', contexts)
 
 def time1(request):
-    return render(request, 'time1.html')
+    contexts = dict()
+    contexts['timeusage'] = visual.timeusage()
+    return render(request, 'time1.html', contexts)
 
-def time2(request):
-    return render(request, 'time2.html')
+def time21(request):
+    contexts = dict()
+    contexts['lifeusage'] = visual.lifeusage()
+    return render(request, 'time21.html', contexts)
+
+def time22(request):
+    contexts = dict()
+    contexts['subusage'] = visual.subusage()
+    return render(request, 'time22.html', contexts)
+
+def time23(request):
+    contexts = dict()
+    contexts['bususage'] = visual.bususage()
+    return render(request, 'time23.html', contexts)
 
 def facilities1(request):
-    return render(request, 'facilities1.html')
+    contexts = dict()
+    return render(request, 'facilities1.html', contexts)
 
 def facilities2(request):
-    return render(request, 'facilities2.html')
+    contexts = dict()
+    return render(request, 'facilities2.html', contexts)
 
 def facilities3(request):
-    return render(request, 'facilities3.html')
+    contexts = dict()
+    return render(request, 'facilities3.html', contexts)
--- seoul_bike/seoul_bike/visual.py
+++ seoul_bike/seoul_bike/visual.py
@@ -2,10 +2,104 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seoul_bike.settings")
 import django
 django.setup()
+
+import plotly.express as px
+import plotly.graph_objects as go
 from seoul_bike.models import *
+import numpy as np
 import pandas as pd
+import plotly.figure_factory as ff
+from plotly.offline import plot
+from plotly.subplots import make_subplots
 
 
+# 따릉이 대여소 갯수 (station_near 기준)
+def countStationId():
+    df = pd.DataFrame(list(StationNear.objects.all().values('station_id')))
+    result = df['station_id'].count()
+    return result
 
-#df2 = pd.DataFrame(list(DongCode.objects.all().values()))
+
+# 따릉이 대여소 top3
+def topStation_id():
+    df = pd.DataFrame(list(StationUsage.objects.all().values('station_id', 'rent_amt', 'return_amt')))
+    df2 = df.groupby(by=['station_id']).sum().reset_index()
+    df2 = df2.sort_values(['rent_amt', 'return_amt'], ascending=False)
+    result = dict()
+    top1 = df2.iloc[0:1]
+    top2 = df2.iloc[1:2]
+    top3 = df2.iloc[2:3]
+    result['top1_station_id'] = top1['station_id'].values[0]
+    result['top1_rent_amt'] = top1['rent_amt'].values[0]
+    result['top1_return_amt'] = top1['return_amt'].values[0]
+    result['top2_station_id'] = top2['station_id'].values[0]
+    result['top2_rent_amt'] = top2['rent_amt'].values[0]
+    result['top2_return_amt'] = top2['return_amt'].values[0]
+    result['top3_station_id'] = top3['station_id'].values[0]
+    result['top3_rent_amt'] = top3['rent_amt'].values[0]
+    result['top3_return_amt'] = top3['return_amt'].values[0]
+    return result
+
+
+# 시간대별 따릉이 이용량
+def timeusage():
+    timeusage = pd.DataFrame(list(TimeUsage.objects.all().values()))
+    fig = go.Figure()
+    # fig = px.line(timeusage, x='base_tm', y='usage_amt', template='plotly_white')
+    fig.add_trace(go.Scatter(x=timeusage.base_tm, y=timeusage.usage_amt, mode='lines',
+                             line={'width': 5}, fill='tozeroy'))
+    fig.update_coloraxes(showscale=False)
+    fig.update_layout(margin=dict(l=10, r=10, t=10, b=10))
+    fig.update_xaxes(title_text="시간대")
+    fig.update_yaxes(title_text="따릉이 이용량")
+    plot_div = plot(fig, output_type='div')
+    return plot_div
+
+
+# 시간대별 생활인구와 따릉이 이용량
+def lifeusage():
+    populusage = pd.DataFrame(list(PopulUsage.objects.all().values()))
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=populusage.base_tm, y=populusage['life_popul'], mode='lines+markers',
+                             line={'width': 7}, marker=dict(color='gold', size=15)), secondary_y=True)
+    fig.add_trace(go.Bar(x=populusage.base_tm, y=populusage['usage_amt'], name="사용량",
+                        marker=dict(color=populusage['usage_amt'], colorscale='teal')), secondary_y=False)
+    fig.update_layout(template='plotly_white', showlegend=False, margin=dict(l=10, r=10, t=10, b=10))
+    fig.update_xaxes(title_text="시간대별 생활인구와 따릉이 이용량", tickangle=45)
+    fig.update_yaxes(title_text="생활인구", secondary_y=True)
+    fig.update_yaxes(title_text="대여소 이용량", secondary_y=False)
+    plot_div = plot(fig, output_type='div')
+    return plot_div
+
+
+# 시간대별 지하철 유동인구와 따릉이 이용량
+def subusage():
+    populusage = pd.DataFrame(list(PopulUsage.objects.all().values()))
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=populusage.base_tm, y=populusage['sub_popul'], mode='lines+markers',
+                            line={'width': 7}, marker=dict(color='gold', size=15)), secondary_y=True)
+    fig.add_trace(go.Bar(x=populusage.base_tm, y=populusage['usage_amt'], name="사용량",
+                        marker=dict(color=populusage['usage_amt'], colorscale='teal')), secondary_y=False)
+    fig.update_layout(template='plotly_white', showlegend=False, margin=dict(l=10, r=10, t=10, b=10))
+    fig.update_xaxes(title_text="시간대별 지하철 유동인구와 따릉이 이용량", tickangle=45)
+    fig.update_yaxes(title_text="지하철 유동인구", secondary_y=True)
+    fig.update_yaxes(title_text="대여소 이용량", secondary_y=False)
+    plot_div = plot(fig, output_type='div')
+    return plot_div
+
+
+# 시간대별 버스 유동인구와 따릉이 이용량
+def bususage():
+    populusage = pd.DataFrame(list(PopulUsage.objects.all().values()))
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=populusage.base_tm, y=populusage['bus_popul'], mode='lines+markers',
+                            line={'width': 7}, marker=dict(color='gold', size=15)), secondary_y=True)
+    fig.add_trace(go.Bar(x=populusage.base_tm, y=populusage['usage_amt'], name="사용량",
+                        marker=dict(color=populusage['usage_amt'], colorscale='teal')), secondary_y=False)
+    fig.update_layout(template='plotly_white', showlegend=False, margin=dict(l=10, r=10, t=10, b=10))
+    fig.update_xaxes(title_text="시간대별 버스 유동인구와 따릉이 이용량", tickangle=45)
+    fig.update_yaxes(title_text="버스 유동인구", secondary_y=True)
+    fig.update_yaxes(title_text="대여소 이용량", secondary_y=False)
+    plot_div = plot(fig, output_type='div')
+    return plot_div
 
